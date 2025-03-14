c1be9fbed7649016f5735b4a38c9f22f9349e74f
Luna Muñoz-Maldonado <156993160+luna-mm@users.noreply.github.com>
2025-03-14 14:29:35 -0500

    Added Check In Page and started with title bar and date

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,30 +9,101 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider(
+      create: (context) => CheckInState(),
+      child: MaterialApp(
+        title: "Vibe Check",
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: CheckInPage()
+      )
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool checkInPending = true;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (checkInPending) {
+      case true:
+        page = CheckInPage();
+        break;
+      case false:
+        page = Placeholder();
+        break;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vibe Check'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: page,
+    );
+  }
+}
+
+class CheckInState extends ChangeNotifier {
+  var checkInTime = DateTime.now();
+}
+
+class CheckInPage extends StatelessWidget {
+  const CheckInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var checkInState = context.watch<CheckInState>();
+    var timestamp = checkInState.checkInTime;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Development Check In Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Vibe Check!",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 40,
+                fontStyle: FontStyle.italic)
+            ),
+            Text.rich(
+              TextSpan(
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+                children: [
+                  WidgetSpan(
+                    child: Icon(Icons.calendar_month)
+                  ),
+                  TextSpan(
+                    text: DateFormat(" MMM d, y ").format(timestamp)
+                  ),
+                  WidgetSpan(
+                    child: Icon(Icons.schedule)
+                  ),
+                  TextSpan(
+                    text: DateFormat(" h:mm a ").format(timestamp)
+                  )
+                ]
+              ))
+          ],
+        ),
+      ),
     );
   }
 }
