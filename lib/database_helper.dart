8c77cbb38c9f524ef10cd6b47e4c4e41de2ea435
Stephanie Miles <smiles1@macalester.edu>
2025-04-11 15:48:08 -0500

    Added retrieveAllEntries

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'entry.dart';
+import 'package:intl/intl.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._instance();
@@ -42,29 +44,83 @@ class DatabaseHelper {
 
   Future<int> updateEntry(Entry entry) async {
     Database db = await instance.db;
-    return await db.update('entries', entry.toMap(), where: 'id = ?', whereArgs: [entry.id.millisecondsSinceEpoch]);
+    return await db.update(
+      'entries',
+      entry.toMap(),
+      where: 'id = ?',
+      whereArgs: [entry.id.millisecondsSinceEpoch],
+    );
   }
 
   Future<int> deleteEntry(DateTime id) async {
     Database db = await instance.db;
-    return await db.delete('entries', where: 'id = ?', whereArgs: [id.millisecondsSinceEpoch]);
+    return await db.delete(
+      'entries',
+      where: 'id = ?',
+      whereArgs: [id.millisecondsSinceEpoch],
+    );
   }
 
   /// For development purposes only
   /// This function initializes the database with some sample entries.
   Future<void> initializeSampleEntries() async {
     List<Entry> entriesToAdd = [
-      Entry(id: DateTime.now().subtract(Duration(days: 1)), emoji: '😊', sentence: 'Finally got the database working!'),
-      Entry(id: DateTime.now().subtract(Duration(days: 2)), emoji: '😔', sentence: 'The database isn\'t working! This sucks :('),
-      Entry(id: DateTime.now().subtract(Duration(days: 3)), emoji: '🫠', sentence: 'Cafe Mac was so bad today.'),
-      Entry(id: DateTime.now().subtract(Duration(days: 4)), emoji: '😒', sentence: 'smh'),
-      Entry(id: DateTime.now().subtract(Duration(days: 5)), emoji: '😡', sentence: 'My computer decided to break down for no reason ugh'),
-      Entry(id: DateTime.now().subtract(Duration(days: 6)), emoji: '🫢', sentence: 'Oops I think I totally got overheard lol'),
-      Entry(id: DateTime.now().subtract(Duration(days: 7)), emoji: '😊', sentence: 'First entry wooh hooooo yayayayyy'),
+      Entry(
+        id: DateTime.now().subtract(Duration(days: 1)),
+        emoji: '😊',
+        sentence: 'Finally got the database working!',
+      ),
+      Entry(
+        id: DateTime.now().subtract(Duration(days: 1)),
+        emoji: '😔',
+        sentence: 'The database isn\'t working! This sucks :(',
+      ),
+      Entry(
+        id: DateTime.now().subtract(Duration(days: 3)),
+        emoji: '🫠',
+        sentence: 'Cafe Mac was so bad today.',
+      ),
+      Entry(
+        id: DateTime.now().subtract(Duration(days: 4)),
+        emoji: '😒',
+        sentence: 'smh',
+      ),
+      Entry(
+        id: DateTime.now().subtract(Duration(days: 5)),
+        emoji: '😡',
+        sentence: 'My computer decided to break down for no reason ugh',
+      ),
+      Entry(
+        id: DateTime.now().subtract(Duration(days: 6)),
+        emoji: '🫢',
+        sentence: 'Oops I think I totally got overheard lol',
+      ),
+      Entry(
+        id: DateTime.now().subtract(Duration(days: 7)),
+        emoji: '😊',
+        sentence: 'First entry wooh hooooo yayayayyy',
+      ),
     ];
 
     for (Entry entry in entriesToAdd) {
       await insertEntry(entry);
     }
+  }
+
+  Future<List<Entry>> retrieveAllEntriesForDay(DateTime givenId) async {
+    List<Entry> entries = [];
+    List<Entry> allDays = [];
+
+    final entryMap = await DatabaseHelper.instance.queryAllEntries();
+    entries = entryMap.map((entryMap) => Entry.fromMap(entryMap)).toList();
+
+    for (var entry in entries) {
+      if (DateUtils.isSameDay(entry.id, givenId)) {
+        allDays.add(entry);
+        print(entry);
+      }
+    }
+
+    return allDays;
   }
 }
